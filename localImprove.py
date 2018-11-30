6bfdc5d0784448dcc56b24bb17cb25781f577bff
Ken Guan <ruihan.guan@you.world>
2018-11-29 22:32:44 -0800

    gooooo

--- localImprove.py
+++ localImprove.py
@@ -58,11 +58,11 @@ def main():
     elif method == 'localImprove_step':
         modify = modify_stepRandom
 
-    for i in range(10):
+    for i in range(100):
         count = 0
         total = 0
 
-        for size in ['small', 'medium', 'large']:
+        for size in ['small']:
             subfolders = [x[1] for x in os.walk('all_inputs/' + size)][0]
             for number in subfolders:
                 graph, num_buses, size_bus, constraints = parse_input(path_to_inputs + '/' + size + '/' + number)
