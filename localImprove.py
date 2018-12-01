585f9f9d3f1b454da74e2ed9498146cff9a832c2
Ken Guan <ruihan.guan@you.world>
2018-11-30 16:01:44 -0800

    size

--- localImprove.py
+++ localImprove.py
@@ -125,7 +125,7 @@ def main():
         count = 0
         total = 0
 
-        for size in ['small']:
+        for size in ['small', 'medium', 'large']:
             subfolders = [x[1] for x in os.walk('all_inputs/' + size)][0]
             for number in subfolders:
                 graph, num_buses, size_bus, constraints = parse_input(path_to_inputs + '/' + size + '/' + number)
